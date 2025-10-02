1ecb5004ebf299f5bab2cd11a2d0ef1a4dcf25d1
Erik Johansson <ejohansson@eh2.com>
2025-10-02 08:36:11 -0700

    Update DATALOG.py

--- DATALOG.py
+++ DATALOG.py
@@ -1,7 +1,12 @@
 # AOI definitions are from PlantPAX V4.10.4
 
 DATALOG_DEFINITIONS = {
-    'P_AIn': ['.Val','.Val_Fault','.Val_InpPV','.SrcQ']
+    'P_AIn': """\
+        GROUPNAME,SHORTCUTTAG.SrcQ,DINT,Periodic,1,Second,,,,,,
+        GROUPNAME,SHORTCUTTAG.Val,REAL,Periodic,1,Second,,,,,,
+        GROUPNAME,SHORTCUTTAG.Val_Fault,DINT,Periodic,1,Second,,,,,,
+        GROUPNAME,SHORTCUTTAG.Val_InpPV,REAL,Periodic,1,Second,,,,,,
+        GROUPNAME,SHORTCUTTAG.Val_Dev,REAL,Periodic,1,Second,,,,,,""",
 
 
 }
